d5010b08e0addc1207916f2913e3a8592de87c07
Dylan Ryman <dylan@dtr-world.com>
2013-12-07 12:46:56 -0500

    Added a game manager

---
+++ src/main/java/ten/java/Quantum64/GameManager.java
@@ -0,0 +1,24 @@
+package ten.java.Quantum64;
+
+public class GameManager {
+
+	private GameState currentState;
+	private Main plugin;
+
+	public GameManager(Main plugin) {
+		this.plugin = plugin;
+		this.currentState = GameState.WAITING_FOR_PLAYERS;
+	}
+
+	public static enum GameState {
+		WAITING_FOR_PLAYERS, STARTING_COUNTDOWN, IN_GAME, POST_GAME
+	}
+
+	public void setState(GameState state) {
+		this.currentState = GameState.WAITING_FOR_PLAYERS;
+	}
+
+	public void onStateChanger() {
+
+	}
+}
--- src/main/java/ten/java/Quantum64/Main.java
+++ src/main/java/ten/java/Quantum64/Main.java
@@ -1,9 +1,18 @@
 package ten.java.Quantum64;
 
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import ten.java.Quantum64.listener.PlayerListener;
 
 public class Main extends JavaPlugin {
 	public void onEnable() {
-
+		Bukkit.getLogger().log(Level.INFO, "[DRAGON WARS] Enabeling plugin - Made by Quantum64 for ten.java code war");
+	}
+	
+	public void setupEvents(){
+		this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
 	}
 }
