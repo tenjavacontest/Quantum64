8a728192d6145e720af3846d59a812705925f246
Dylan Ryman <dylan@dtr-world.com>
2013-12-07 15:18:55 -0500

    Added CB code to make the dragon sat in one piece

--- src/main/java/ten/java/Quantum64/entity/EntityDragon.java
+++ src/main/java/ten/java/Quantum64/entity/EntityDragon.java
@@ -1,10 +1,21 @@
 package ten.java.Quantum64.entity;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import net.minecraft.server.v1_7_R1.DamageSource;
+import net.minecraft.server.v1_7_R1.EntityComplexPart;
 import net.minecraft.server.v1_7_R1.EntityEnderDragon;
-
+import net.minecraft.server.v1_7_R1.MathHelper;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_4_5.CraftWorld;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import ten.java.Quantum64.Main;
 
@@ -18,7 +29,7 @@ public class EntityDragon extends EntityEnderDragon {
 
 	Main plugin;
 	byte upDown = 0;
-	boolean br = true;
+	boolean brI = true;
 	boolean ignoreSneaking = false;
 	double fl = 0.0D;
 
@@ -28,6 +39,9 @@ public class EntityDragon extends EntityEnderDragon {
 		this.player = player.getName();
 	}
 
+	/**
+	 * This is some CB code needed to make the entity
+	 */
 	public void c() {
 
 		if (this.passenger == null) {
@@ -41,7 +55,7 @@ public class EntityDragon extends EntityEnderDragon {
 				else
 					setPosition(this.locX, myY, this.locZ);
 			}
-			// I think I need to do something here for the dragon's rotation?();
+			d();
 			return;
 		}
 		this.onGround = false;
@@ -59,14 +73,14 @@ public class EntityDragon extends EntityEnderDragon {
 				this.ignoreSneaking = false;
 			}
 		} else if (p.isSneaking()) {
-			if (!this.br)
-				this.br = true;
+			if (!this.brI)
+				this.brI = true;
 			else
-				this.br = false;
+				this.brI = false;
 			this.ignoreSneaking = true;
 		}
 
-		if (this.br) {
+		if (this.brI) {
 			return;
 		}
 		if (this.fl > 0.0D) {
@@ -167,6 +181,70 @@ public class EntityDragon extends EntityEnderDragon {
 		}
 
 		setPosition(myX, myY, myZ);
-		// I need to do something else here();
+		d();
 	}
+
+	private void d() {
+		this.aN = this.yaw;
+		this.bq.width = this.bq.length = 3.0F;
+		this.bs.width = this.bs.length = 2.0F;
+		this.bt.width = this.bt.length = 2.0F;
+		this.bu.width = this.bu.length = 2.0F;
+		this.br.length = 3.0F;
+		this.br.width = 5.0F;
+		this.bv.length = 2.0F;
+		this.bv.width = 4.0F;
+		this.bw.length = 3.0F;
+		this.bw.width = 4.0F;
+		float f1 = (float) (this.b(5, 1.0F)[1] - this.b(10, 1.0F)[1]) * 10.0F / 180.0F * 3.1415927F;
+		float f2 = MathHelper.cos(f1);
+		float f9 = -MathHelper.sin(f1);
+		float f10 = this.yaw * 3.1415927F / 180.0F;
+		float f11 = MathHelper.sin(f10);
+		float f12 = MathHelper.cos(f10);
+
+		this.br.h();
+		this.br.setPositionRotation(this.locX + (double) (f11 * 0.5F), this.locY, this.locZ - (double) (f12 * 0.5F), 0.0F, 0.0F);
+		this.bv.h();
+		this.bv.setPositionRotation(this.locX + (double) (f12 * 4.5F), this.locY + 2.0D, this.locZ + (double) (f11 * 4.5F), 0.0F, 0.0F);
+		this.bw.h();
+		this.bw.setPositionRotation(this.locX - (double) (f12 * 4.5F), this.locY + 2.0D, this.locZ - (double) (f11 * 4.5F), 0.0F, 0.0F);
+
+		double[] adouble = this.b(5, 1.0F);
+		double[] adouble1 = this.b(0, 1.0F);
+
+		float f3 = MathHelper.sin(this.yaw * 3.1415927F / 180.0F - this.bg * 0.01F);
+		float f13 = MathHelper.cos(this.yaw * 3.1415927F / 180.0F - this.bg * 0.01F);
+
+		this.bq.h();
+		this.bq.setPositionRotation(this.locX + (double) (f3 * 5.5F * f2), this.locY + (adouble1[1] - adouble[1]) * 1.0D + (double) (f9 * 5.5F), this.locZ - (double) (f13 * 5.5F * f2), 0.0F, 0.0F);
+
+		for (int j = 0; j < 3; ++j) {
+			EntityComplexPart entitycomplexpart = null;
+
+			if (j == 0) {
+				entitycomplexpart = this.bs;
+			}
+
+			if (j == 1) {
+				entitycomplexpart = this.bt;
+			}
+
+			if (j == 2) {
+				entitycomplexpart = this.bu;
+			}
+
+			double[] adouble2 = this.b(12 + j * 2, 1.0F);
+			float f14 = this.yaw * 3.1415927F / 180.0F + (float) MathHelper.g(adouble2[0] - adouble[0]) * 3.1415927F / 180.0F * 1.0F;
+			float f15 = MathHelper.sin(f14);
+			float f16 = MathHelper.cos(f14);
+			float f17 = 1.5F;
+			float f18 = (float) (j + 1) * 2.0F;
+
+			entitycomplexpart.h();
+			entitycomplexpart.setPositionRotation(this.locX - (double) ((f11 * f17 + f15 * f18) * f2), this.locY + (adouble2[1] - adouble[1]) * 1.0D - (double) ((f18 + f17) * f9) + 1.5D, this.locZ + (double) ((f12 * f17 + f16 * f18) * f2), 0.0F, 0.0F);
+		}
+
+	}
+
 }
