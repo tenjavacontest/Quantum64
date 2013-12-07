7c7037946a633a29b48692849b42315e6af412aa
Dylan Ryman <dylan@dtr-world.com>
2013-12-07 11:28:40 -0500

    Wait forgot the constructor

--- src/main/java/ten/java/Quantum64/entity/EntityDragon.java
+++ src/main/java/ten/java/Quantum64/entity/EntityDragon.java
@@ -1,5 +1,16 @@
 package ten.java.Quantum64.entity;
 
-public class EntityDragon {
+import net.minecraft.server.v1_7_R1.EntityEnderDragon;
+
+import org.bukkit.entity.Player;
+
+import ten.java.Quantum64.Main;
+
+public class EntityDragon extends EntityEnderDragon {
+
+	public EntityDragon(Main plugin, Player player, net.minecraft.server.v1_7_R1.World world) {
+		super(world);
+
+	}
 
 }
